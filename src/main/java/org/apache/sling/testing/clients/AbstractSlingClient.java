78694965d00c657155b49e21060858ee558bb47e
Thierry Ygé <tyge@adobe.com>
2020-02-09 11:58:51 +0100

    SLING-9053 revert to original

--- src/main/java/org/apache/sling/testing/clients/AbstractSlingClient.java
+++ src/main/java/org/apache/sling/testing/clients/AbstractSlingClient.java
@@ -33,6 +33,7 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
