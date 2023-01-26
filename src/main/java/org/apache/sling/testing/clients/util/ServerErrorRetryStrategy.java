a11c4534e31f479982c635a2f306f0e557659e7a
Marc Pfaff <pfaffm@adobe.com>
2023-01-26 10:19:49 +0100

    SLING-11748 - Improve logging output of HTTP retries in testing clients
    - wrap lines at 120 characters

--- src/main/java/org/apache/sling/testing/clients/util/ServerErrorRetryStrategy.java
+++ src/main/java/org/apache/sling/testing/clients/util/ServerErrorRetryStrategy.java
@@ -39,6 +39,7 @@ import static org.apache.sling.testing.Constants.EXPECTED_STATUS;
 public class ServerErrorRetryStrategy implements ServiceUnavailableRetryStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(ServerErrorRetryStrategy.class);
+    private Collection<Integer> httpRetriesErrorCodes;
 
     public ServerErrorRetryStrategy() {
         super();
@@ -47,11 +48,13 @@ public class ServerErrorRetryStrategy implements ServiceUnavailableRetryStrategy
     @Override
     public boolean retryRequest(final HttpResponse response, final int executionCount, final HttpContext context) {
         int[] expectedStatus = (int[]) context.getAttribute(EXPECTED_STATUS);
-        boolean needsRetry = executionCount <= SystemPropertiesConfig.getHttpRetries() && responseRetryCondition(response, expectedStatus);
+        boolean needsRetry = executionCount <= SystemPropertiesConfig.getHttpRetries() &&
+                responseRetryCondition(response, expectedStatus);
 
         if (SystemPropertiesConfig.isHttpLogRetries() && needsRetry && LOG.isWarnEnabled()) {
             LOG.warn("Request retry condition met: [count={}/{}], [expected-codes={}], [retry-codes={}]",
-                    executionCount, SystemPropertiesConfig.getHttpRetries(), expectedStatus, SystemPropertiesConfig.getHttpRetriesErrorCodes());
+                    executionCount, SystemPropertiesConfig.getHttpRetries(), expectedStatus,
+                    httpRetriesErrorCodes);
             LOG.warn("Request: {}", getRequestDetails(context));
             LOG.warn("Response: {}", getResponseDetails(response));
             try {
@@ -93,10 +96,12 @@ public class ServerErrorRetryStrategy implements ServiceUnavailableRetryStrategy
         HttpRequestWrapper wrapper = clientContext.getAttribute(HttpClientContext.HTTP_REQUEST, HttpRequestWrapper.class);
         if (wrapper != null) {
             // Build a request detail string like following example:
-            // GET /test/internalerror/resource HTTP/1.1 [Host: 127.0.0.1:35049, Connection: Keep-Alive, User-Agent: Java, Accept-Encoding: gzip,deflate, Authorization: Basic dXNlcjpwYXNz]
+            // GET /test/internalerror/resource HTTP/1.1 [Host: 127.0.0.1:35049, Connection: Keep-Alive, User-Agent: Java,
+            //   Accept-Encoding: gzip,deflate, Authorization: Basic dXNlcjpwYXNz]
             final StringBuilder sb = new StringBuilder(wrapper.getRequestLine().toString());
             sb.append(" [");
-            Arrays.stream(wrapper.getAllHeaders()).forEach(header -> sb.append(header.getName()).append(": ").append(header.getValue()).append(", "));
+            Arrays.stream(wrapper.getAllHeaders()).forEach(header ->
+                    sb.append(header.getName()).append(": ").append(header.getValue()).append(", "));
             sb.append("]");
             details = sb.toString();
 
@@ -111,10 +116,12 @@ public class ServerErrorRetryStrategy implements ServiceUnavailableRetryStrategy
         String details = "Not available";
         if (response != null) {
             // Build a response string like following example:
-            // HTTP/1.1 500 Internal Server Error [Date: Thu, 12 Jan 2023 08:32:42 GMT, Server: TEST/1.1, Content-Length: 8, Content-Type: text/plain; charset=ISO-8859-1, Connection: Keep-Alive, ]
+            // HTTP/1.1 500 Internal Server Error [Date: Thu, 12 Jan 2023 08:32:42 GMT, Server: TEST/1.1, 
+            //   Content-Length: 8, Content-Type: text/plain; charset=ISO-8859-1, Connection: Keep-Alive, ]
             final StringBuilder sb = new StringBuilder(response.getStatusLine().toString());
             sb.append(" [");
-            Arrays.stream(response.getAllHeaders()).forEach(h -> sb.append(h.getName()).append(": ").append(h.getValue()).append(", "));
+            Arrays.stream(response.getAllHeaders()).forEach(h ->
+                    sb.append(h.getName()).append(": ").append(h.getValue()).append(", "));
             sb.append("]");
             details = sb.toString();
         }
