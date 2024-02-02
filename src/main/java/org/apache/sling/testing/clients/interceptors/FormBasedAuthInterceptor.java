d5ea558f7b894b970b8c807028defaede7a5c8f9
Evgeny Tugarev <tugarev@adobe.com>
2024-02-02 13:54:30 +0100

    removed unused import

--- src/main/java/org/apache/sling/testing/clients/interceptors/FormBasedAuthInterceptor.java
+++ src/main/java/org/apache/sling/testing/clients/interceptors/FormBasedAuthInterceptor.java
@@ -16,7 +16,6 @@
  */
 package org.apache.sling.testing.clients.interceptors;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
